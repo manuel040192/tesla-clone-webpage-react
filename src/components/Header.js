86bd93dbc4d52f034aefd2a1ef019a9a66a03339
Manuel Arce <90724931+manuel040192@users.noreply.github.com>
2021-10-27 23:15:45 -0500

    Add files via upload

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import styled from 'styled-components';
+import CloseIcon from '@mui/icons-material/Close';
+import MenuIcon from '@mui/icons-material/Menu';
 
 function Header() {
     return (
@@ -20,6 +22,25 @@ function Header() {
                 <p><a href='#'>Account</a></p>
                 <p><a href='#'>Menu</a></p>
             </RightMenu>
+            <BurgerNav>
+                <CloseWrapper>
+                    <CustomClose />
+                </CloseWrapper>
+                <li><a href='#'>Existing Inventory</a></li>
+                <li><a href='#'>Used Inventory</a></li>
+                <li><a href='#'>Trade-In</a></li>
+                <li><a href='#'>Test Drive</a></li>
+                <li><a href='#'>Cybertruck</a></li>
+                <li><a href='#'>Roadster</a></li>
+                <li><a href='#'>Semi</a></li>
+                <li><a href='#'>Charging</a></li>
+                <li><a href='#'>Powerwall</a></li>
+                <li><a href='#'>Commercial Energy</a></li>
+                <li><a href='#'>Utilities</a></li>
+                <li><a href='#'>Find Us</a></li>
+                <li><a href='#'>Support</a></li>
+                <li><a href='#'>Investor Relations</a></li>
+            </BurgerNav>
         </Container>
     )
 }
@@ -33,7 +54,8 @@ const Container = styled.div`
     align-items: center;
     padding: 0 20px;
     left: 0;
-    right: 0; 
+    right: 0;
+    z-index: 1;
 `
 
 const Menu = styled.div`
@@ -60,4 +82,33 @@ const RightMenu = styled.div`
         padding: 0 10px;
         flex-wrap: nowrap;
     }
+`
+const BurgerNav = styled.div`
+    position: fixed;
+    top: 0;
+    bottom: 0;
+    right: 0;
+    background: white;
+    width: 300px;
+    z-index: 10
+    list-style: none;
+    padding: 20px;
+    display: flex;
+    flex-direction: column;
+    justify-content: flex-start;
+    text-align: start;
+    li {
+        padding: 15px 0;
+        border-bottom: 1px solid rgba(0, 0, 0, .2);
+        
+        a {
+            font-weight: 600;
+        }
+    }
+`
+const CustomClose = styled(CloseIcon)``
+
+const CloseWrapper = styled.div`
+    display: flex;
+    justify-content: flex-end;
 `
