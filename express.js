7f407b3b66b66310bcefac45393eda12ec5429b8
Manuel Arce <90724931+manuel040192@users.noreply.github.com>
2021-11-09 03:37:03 -0500

    Update express.js

--- express.js
+++ express.js
@@ -6,6 +6,7 @@ app.use(express.static(path.join(__dirname, 'build')));
 
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
+  res.end()
 });
 
 const PORT = process.env.PORT ||5000;
