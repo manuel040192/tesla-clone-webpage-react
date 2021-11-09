dac949e9abd299a2ca1aa82d77f98b339eb9bd3f
Manuel Arce <90724931+manuel040192@users.noreply.github.com>
2021-11-09 03:09:48 -0500

    Update express.js

--- express.js
+++ express.js
@@ -1,4 +1,3 @@
-// Importing express module
 const express = require('express');
 const path = require('path');
 const app = express();
@@ -9,10 +8,8 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
-// Establishing the port
 const PORT = process.env.PORT ||5000;
 
-// Executing the server on given port number
 app.listen(PORT, console.log(
 `Server started on port ${PORT}`));
 
