315f14b43c7e6e9170f4faa1bb98fb31b36689c3
Manuel Arce <90724931+manuel040192@users.noreply.github.com>
2021-12-20 10:24:18 -0500

    Update server.js

--- server.js
+++ server.js
@@ -1,12 +1,12 @@
 const express = require("express")
 const app = express()
-const PORT = process.env.PORT || 8080;
-app.listen(8080, () => console.log('Server running on port {PORT}'))
-const path = require('path');
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static( 'build' ));
+const PORT = process.env.PORT || 3000
+const path = require('path')
+                    
+app.listen(3000, () => console.log('Server running on port {PORT}'))
 
-  app.get('*', (req, res) => {
-      res.sendFile(path.join(__dirname, 'build', 'index.html')); 
-  });
-}
+app.use(express.static(path.join(__dirname, 'public')))
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'))
+})
