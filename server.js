60f3b890a06c3a1c41dccb489d80c322324d26f0
Manuel Arce <90724931+manuel040192@users.noreply.github.com>
2021-12-19 20:11:27 -0500

    Update server.js

--- server.js
+++ server.js
@@ -2,77 +2,13 @@ require('dotenv').config();
 
 const express = require("express")
 const app = express()
-const PORT = process.env.PORT || 5000;
-app.listen(5000, () => console.log('Server running on port {PORT}'))
+const PORT = process.env.PORT || 8080;
+app.listen(8080, () => console.log('Server running on port {PORT}'))
 const path = require('path');
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static( 'public/build' ));
 
   app.get('*', (req, res) => {
-      res.sendFile(path.join(__dirname, 'client', 'build', 'index.html')); // relative path
+      res.sendFile(path.join(__dirname, 'build', 'index.html')); // relative path
   });
 }
-
-
-/* const paypal = require("@paypal/checkout-server-sdk")
-const Environment =
-  process.env.NODE_ENV === "production"
-    ? paypal.core.LiveEnvironment
-    : paypal.core.SandboxEnvironment
-const paypalClient = new paypal.core.PayPalHttpClient(
-  new Environment(
-    process.env.PAYPAL_CLIENT_ID,
-    process.env.PAYPAL_CLIENT_SECRET
-  )
-)
-
-
-app.get("/", (req, res) => {
-  res.render("index", {
-    paypalClientId: process.env.PAYPAL_CLIENT_ID,
-  })
-})
-
-
-app.post("/create-order", async (req, res) => {
-  const request = new paypal.orders.OrdersCreateRequest()
-  const total = req.body.items.reduce((sum, item) => {
-    return sum + storeItems.get(item.id).price * item.quantity
-  }, 0)
-  request.prefer("return=representation")
-  request.requestBody({
-    intent: "CAPTURE",
-    purchase_units: [
-      {
-        amount: {
-          currency_code: "USD",
-          value: total,
-          breakdown: {
-            item_total: {
-              currency_code: "USD",
-              value: total,
-            },
-          },
-        },
-        items: req.body.items.map(item => {
-          const storeItem = storeItems.get(item.id)
-          return {
-            name: storeItem.name,
-            unit_amount: {
-              currency_code: "USD",
-              value: storeItem.price,
-            },
-            quantity: item.quantity,
-          }
-        }),
-      },
-    ],
-  })
-
-  try {
-    const order = await paypalClient.execute(request)
-    res.json({ id: order.result.id })
-  } catch (e) {
-    res.status(500).json({ error: e.message })
-  }
-}) */
