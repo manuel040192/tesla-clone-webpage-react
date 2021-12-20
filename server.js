cdcb88fbb40f44deb8341458aff423812db529db
Manuel Arce <90724931+manuel040192@users.noreply.github.com>
2021-12-19 20:11:47 -0500

    Update server.js

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const PORT = process.env.PORT || 8080;
 app.listen(8080, () => console.log('Server running on port {PORT}'))
 const path = require('path');
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static( 'public/build' ));
+  app.use(express.static( 'build' ));
 
   app.get('*', (req, res) => {
       res.sendFile(path.join(__dirname, 'build', 'index.html')); // relative path
